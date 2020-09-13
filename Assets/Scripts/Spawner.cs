2b7d3ba51ca95c3a8c46d6470830e0df6b92c9fc
kirill karev <karev.kirill.a@gmail.com>
2020-09-13 19:52:49 +0300

    Test merge

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public gameObject SpawnObject;
+    public GameObject spawnObject;
     public float spawnTime = 1f;
+
     private float timer = 0;
 
     void Start()
     {
-        
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        spawnTime -= Time.deltaTime;
-        if (spawnTime <= 0)
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Instantiate(spawnObject, transform.position, transform.rotation);
+
+            timer = spawnTime;
+        }
     }
 }
